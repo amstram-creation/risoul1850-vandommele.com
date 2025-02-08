1b0b14af27e418628d0ae52a959517d4920a0b38
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-02-08 23:10:04 +0100

    simplifying fetching and rendering logic

--- js/ReservationSheet.js
+++ js/ReservationSheet.js
@@ -1,13 +1,46 @@
 export default class ReservationSheet {
-  constructor(sheetId, sheetName) {
-    this.extractor = new GoogleSheetExtractor(sheetId, sheetName);
-    this.fresh = false;
-    this.ui = new ReservationUI('#calendar-container');
-    this.refreshReservations();
+  constructor(selector, sheetId, sheetName) {
+    this.container = document.querySelector(selector);
+    this.url = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:json&sheet=${sheetName}`;
+    this.render();
   }
 
-  async refreshReservations() {
-    const reservations = await this.extractor.fetchReservations();
+  render() {
+    let _;
+    _ = this.nextSaturday();
+    _ = this.makeReservationList(_, 12);
+    this.container.innerHTML = '';
+    this.container.appendChild(_);
+
+    return this;
+  }
+
+  async fetch() {
+    const regex = /\{(?:[^{}]|\{(?:[^{}]|\{[^{}]*\})*\})*\}/s;
+
+    let _;
+
+    try {
+      console.log('Fetching reservations...', this.url);
+      _ = await fetch(this.url);
+      _ = await _.text();
+      _ = _.match(regex);
+      if (_ && _[0]) {
+        _ = JSON.parse(_[0]);
+
+        return (_?.rows || [])
+          .filter((row) => row?.c[0]?.f)
+          .map((row) => row.c[0].f); // Extract the valid date values
+      }
+    } catch (error) {
+      console.error('Error fetching reservations:', error);
+    }
+
+    return null;
+  }
+
+  async hydrate() {
+    let reservations = await this.fetch();
     console.log('Reservations:', reservations);
     //iterate on the set
     if (reservations === null) {
@@ -16,9 +49,11 @@ export default class ReservationSheet {
     }
     reservations.forEach((date) => {
       const cell = document.querySelector(`[data-date="${date}"]:not(.booked)`);
-      cell.textContent = 'Réservé';
-      cell.classList.add('booked');
-      cell.setAttribute('aria-label', 'Réservé');
+      if (cell) {
+        cell.textContent = 'Réservé';
+        cell.classList.add('booked');
+        cell.setAttribute('aria-label', 'Réservé');
+      }
     });
 
     document.querySelectorAll('[data-date]:not(.booked)').forEach((cell) => {
@@ -35,24 +70,9 @@ export default class ReservationSheet {
       cell.appendChild(bookingLink);
     });
   }
-}
-
-class ReservationUI {
-  constructor(selector) {
-    const container = document.querySelector(selector);
-    container.innerHTML = '';
-
-    let currentDate = new Date();
-    if (currentDate.getDay() !== 6) {
-      currentDate.setDate(currentDate.getDate() + (6 - currentDate.getDay()));
-    }
-    let _ = this.makeReservationList(currentDate, 12);
-    container.appendChild(_);
-  }
 
   makeReservationList(currentDate, numWeeks) {
     let _;
-    let ISODate, frenchDate, price, listItem;
     const formatter = new Intl.DateTimeFormat('fr-FR', {
       day: 'numeric',
       month: 'long',
@@ -60,45 +80,30 @@ class ReservationUI {
     const fragment = document.createDocumentFragment();
 
     for (let i = 0; i < numWeeks; ++i) {
-      ISODate = this.formatDateToISO(currentDate);
-      frenchDate = `Semaine du ${formatter.format(currentDate)}`;
-      price = this.isHighSeason(ISODate) ? '1370 €' : '830 €';
+      const ISODate = this.formatDateToISO(currentDate);
+      const frenchDate = `Semaine du ${formatter.format(currentDate)}`;
+      const price = this.isHighSeason(ISODate) ? '1370 €' : '830 €';
 
-      listItem = document.createElement('li');
-      listItem.classList.add('booking-item');
-
-      _ = document.createElement('span');
-      _.classList.add('week');
-      _.setAttribute(
-        'aria-label',
-        `Semaine commençant le ${formatter.format(currentDate)}`
+      fragment.appendChild(
+        Object.assign(document.createElement('li'), {
+          className: 'booking-item',
+          innerHTML: `
+          <span class="week">${frenchDate}</span>
+          <span class="price" aria-label="Prix : ${price}">${price}</span>
+          <a href="mailto:info@risoul1850-vandommele.com?subject=Réservation%20pour%20la%20semaine%20du%20${ISODate}" 
+             target="_blank" 
+             class="book-now" 
+             data-date="${ISODate}" 
+             aria-label="Réserver pour la semaine du ${ISODate}">Réserver</a>
+        `,
+        })
       );
-      _.textContent = frenchDate;
-
-      listItem.appendChild(_);
-
-      _ = document.createElement('span');
-      _.classList.add('price');
-      _.setAttribute('aria-label', `Prix : ${price}`);
-      _.textContent = price;
-      listItem.appendChild(_);
-
-      _ = document.createElement('a');
-      _.href = `mailto:info@risoul1850-vandommele.com?subject=Réservation%20pour%20la%20semaine%20du%20${ISODate}`;
-      _.target = '_blank';
-      _.classList.add('book-now');
-      _.dataset.date = ISODate;
-      _.setAttribute('aria-label', `Réserver pour la semaine du ${ISODate}`);
-      _.textContent = 'Réserver';
-      listItem.appendChild(_);
-
-      fragment.appendChild(listItem);
 
       currentDate.setDate(currentDate.getDate() + 7);
     }
+
     _ = document.createElement('ul');
     _.appendChild(fragment);
-
     return _;
   }
 
@@ -122,47 +127,10 @@ class ReservationUI {
     const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
   }
-}
 
-class GoogleSheetExtractor {
-  constructor(sheetId, sheetName) {
-    this.sheetId = sheetId;
-    this.sheetName = sheetName;
-    this.apiUrl = `https://docs.google.com/spreadsheets/d/${this.sheetId}/gviz/tq?tqx=out:json&sheet=${this.sheetName}`;
-  }
-
-  async fetchReservations() {
-    console.log('Fetching reservations...', this.apiUrl);
-    let _;
-    try {
-      _ = await fetch(this.apiUrl);
-      _ = await _.text();
-      return this.extractReservationDates(_);
-    } catch (error) {
-      console.error('Error fetching reservations:', error);
-    }
-    return null;
-  }
-
-  extractReservationDates(_) {
-    const regex = /\{(?:[^{}]|\{(?:[^{}]|\{[^{}]*\})*\})*\}/s;
-    
-    _ = _.match(regex);
-    if (_ && _[0]) {
-      try {
-        _ = JSON.parse(_[0]);
-    console.log(_.rows);
-
-        return (_?.rows || [])
-          .filter(
-            (row) =>
-              row?.c[0]?.f
-          )
-          .map((row) => row.c[0].f); // Extract the valid date values
-      } catch (error) {
-        console.error('Invalid JSON extracted:', error);
-      }
-    }
-    return null;
+  nextSaturday(date = null) {
+    let _ = date || new Date();
+    _.setDate(_.getDate() + (6 - _.getDay()));
+    return _;
   }
 }
